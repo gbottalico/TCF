cef74620c069c728e7f7c708e9adbb609c28d258
Davide Leone <davideleone94@gmail.com>
2017-12-05 18:09:03 +0100

    Adattamento Login a primeNG

---
+++ tcfWebClient/src/app/view/login/login.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+//import { AppRoutingModule }     from '../../app-routing.module';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './login.component';
+import {InputTextModule} from 'primeng/primeng';
+
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    NgbModule.forRoot(),
+    FormsModule,
+    InputTextModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  exports: [LoginComponent],
+  bootstrap: [LoginComponent]
+})
+export class LoginModule { }
